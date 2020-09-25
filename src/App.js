f2b7ad6c78782fb295967993bdf598f56d955b90
jfitzsimmons <jfitzhome@gmail.com>
2020-09-25 13:36:47 -0500

    prettier and lint updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
 
-import React, { Component } from 'react';
-import './App.scss';
-import { TimerLengthControl } from './Control.js';
+import React, { Component } from "react";
+import "./App.scss";
+import { TimerLengthControl } from "./Control.js";
 
 class App extends Component {
   constructor(props) {
@@ -12,12 +11,12 @@ class App extends Component {
     this.state = {
       brkLength: 5,
       seshLength: 10,
-      timerState: 'stopped',
-      timerType: 'Session',
+      timerState: "stopped",
+      timerType: "Session",
       timer: 600,
-      intervalID: '',
-      alarmColor: {color: '#222', borderColor: 'hsla(13, 98%, 49%, 0.2)'}
-    }
+      intervalID: "",
+      alarmColor: { color: "#222", borderColor: "hsla(13, 98%, 49%, 0.2)" },
+    };
     this.setBrkLength = this.setBrkLength.bind(this);
     this.setSeshLength = this.setSeshLength.bind(this);
     this.lengthControl = this.lengthControl.bind(this);
@@ -33,33 +32,42 @@ class App extends Component {
     this.reset = this.reset.bind(this);
   }
   setBrkLength(e) {
-    this.lengthControl('brkLength', e.currentTarget.value,
-    this.state.brkLength, 'Session');
+    this.lengthControl(
+      "brkLength",
+      e.currentTarget.value,
+      this.state.brkLength,
+      "Session"
+    );
   }
   setSeshLength(e) {
-    this.lengthControl('seshLength', e.currentTarget.value,
-    this.state.seshLength, 'Break');
+    this.lengthControl(
+      "seshLength",
+      e.currentTarget.value,
+      this.state.seshLength,
+      "Break"
+    );
   }
   lengthControl(stateToChange, sign, currentLength, timerType) {
-    if (this.state.timerState === 'running') return;
-        this.setState({[stateToChange]: sign,
-          timer: sign * 60});
+    if (this.state.timerState === "running") return;
+    this.setState({ [stateToChange]: sign, timer: sign * 60 });
   }
 
   timerControl() {
-    let control = this.state.timerState === 'stopped' ? (
-      this.beginCountDown(),
-      this.setState({
-        timerState: 'running',
-        alarmColor: {color: '#222', borderColor: 'hsla(13, 98%, 49%, 1)'}
-      })
-    ) : (
-      this.state.intervalID && clearInterval(this.state.intervalID),
-      this.setState({
-        timerState: 'stopped',
-        alarmColor: {color: '#222', borderColor: 'hsla(13, 98%, 49%, .2)'}
-      })
-    );
+    let control =
+      this.state.timerState === "stopped"
+        ? (this.beginCountDown(),
+          this.setState({
+            timerState: "running",
+            alarmColor: { color: "#222", borderColor: "hsla(13, 98%, 49%, 1)" },
+          }))
+        : (this.state.intervalID && clearInterval(this.state.intervalID),
+          this.setState({
+            timerState: "stopped",
+            alarmColor: {
+              color: "#222",
+              borderColor: "hsla(13, 98%, 49%, .2)",
+            },
+          }));
   }
   accurateInterval() {
     this.decrementTimer();
@@ -67,37 +75,36 @@ class App extends Component {
   }
   beginCountDown() {
     this.setState({
-      intervalID: setInterval(this.accurateInterval.bind(this), 1000)
-    })
+      intervalID: setInterval(this.accurateInterval.bind(this), 1000),
+    });
   }
   decrementTimer() {
-    if (this.state.timerType === 'Session') {
-      this.setState({timer: this.state.timer - 1});
+    if (this.state.timerType === "Session") {
+      this.setState({ timer: this.state.timer - 1 });
     } else {
-      this.setState({timer: this.state.timer + 1});
+      this.setState({ timer: this.state.timer + 1 });
     }
   }
   phaseControl() {
     let timer = this.state.timer;
-    
+
     //this.buzzer(timer);
     if (timer < 0) {
-      this.state.timerType === 'Session' ? (
-        this.state.intervalID && clearInterval(this.state.intervalID),
-        this.beginCountDown(),
-        this.switchTimer(0, 'Break'),
-        this.warning(timer)
-      ) : (
-        this.state.intervalID && clearInterval(this.state.intervalID),
-        this.beginCountDown(),
-        this.switchTimer(this.state.seshLength * 60, 'Session')
-      );
+      this.state.timerType === "Session"
+        ? (this.state.intervalID && clearInterval(this.state.intervalID),
+          this.beginCountDown(),
+          this.switchTimer(0, "Break"),
+          this.warning(timer))
+        : (this.state.intervalID && clearInterval(this.state.intervalID),
+          this.beginCountDown(),
+          this.switchTimer(this.state.seshLength * 60, "Session"));
     }
   }
   warning(_timer) {
-    let warn = _timer < 61 ?
-    this.setState({alarmColor: {color: '#a50d0d'}}) :
-    this.setState({alarmColor: {color: '#222'}});
+    let warn =
+      _timer < 61
+        ? this.setState({ alarmColor: { color: "#a50d0d" } })
+        : this.setState({ alarmColor: { color: "#222" } });
   }
   buzzer(_timer) {
     if (_timer === 0) {
@@ -108,38 +115,35 @@ class App extends Component {
     this.setState({
       timer: num,
       timerType: str,
-      alarmColor: {color: '#222'}
-    })
+      alarmColor: { color: "#222" },
+    });
   }
   clockify() {
     let minutes = Math.floor(this.state.timer / 60);
     let seconds = this.state.timer - minutes * 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    return minutes + ':' + seconds;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    return minutes + ":" + seconds;
   }
   onKeyUp(e) {
-    
     if (e.charCode === 13) {
-      console.log('onKeyUp inside ENTER');
+      console.log("onKeyUp inside ENTER");
       this.reset();
       console.log(`1 this.state.timerState: ${this.state.timerState}`);
       this.timerControl();
-        
-        console.log(`2 this.state.timerState: ${this.state.timerState}`);
 
+      console.log(`2 this.state.timerState: ${this.state.timerState}`);
     }
-      
   }
   reset() {
     let currentSeshLength = this.state.seshLength;
-    console.log(`currentSeshLength * 60 - RESet: ${currentSeshLength}`)
+    console.log(`currentSeshLength * 60 - RESet: ${currentSeshLength}`);
     this.setState({
-      timerState: 'stopped',
-      timerType: 'Session',
+      timerState: "stopped",
+      timerType: "Session",
       timer: currentSeshLength * 60,
-      intervalID: '',
-      alarmColor: {color: '#222'}
+      intervalID: "",
+      alarmColor: { color: "#222" },
     });
     this.state.intervalID && clearInterval(this.state.intervalID);
   }
@@ -147,32 +151,36 @@ class App extends Component {
     return (
       <div>
         <TimerLengthControl
-          titleID="session-label"   minID="session-decrement"
-          addID="session-increment" lengthID="session-length"
-          title="Minutes"    onClick={this.setSeshLength}
-          length={this.state.seshLength} clickEnter={this.onKeyUp} />
+          titleID="session-label"
+          minID="session-decrement"
+          addID="session-increment"
+          lengthID="session-length"
+          title="Minutes"
+          onClick={this.setSeshLength}
+          length={this.state.seshLength}
+          clickEnter={this.onKeyUp}
+        />
         <div className="timer" style={this.state.alarmColor}>
           <div className="timer-wrapper">
-            <div id='timer-label'>
-              {this.state.timerType}
-            </div>
-            <div id='time-left'>
-              {this.clockify()}
-            </div>
+            <div id="timer-label">{this.state.timerType}</div>
+            <div id="time-left">{this.clockify()}</div>
           </div>
         </div>
         <div className="timer-control">
-          <button id="start_stop" onClick={this.timerControl}
-            onKeyPress={this.onKeyUp}>
-            <i className="fa fa-play fa-2x"/>
-            <i className="fa fa-pause fa-2x"/>
+          <button
+            id="start_stop"
+            onClick={this.timerControl}
+            onKeyPress={this.onKeyUp}
+          >
+            <i className="fa fa-play fa-2x" />
+            <i className="fa fa-pause fa-2x" />
           </button>
           <button id="reset" onClick={this.reset}>
             <i className="fas fa-redo fa-2x"></i>
           </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Control.js
+++ src/Control.js
@@ -1,25 +1,27 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
 
 export class TimerLengthControl extends Component {
-render() {
-  return (
-    <div className="length-control">
-      <div id={this.props.titleID}>
-        {this.props.title}
+  render() {
+    return (
+      <div className="length-control">
+        <div id={this.props.titleID}>{this.props.title}</div>
+        <div>
+          <input
+            id={this.props.minID}
+            type="number"
+            className="btn-level"
+            defaultValue={this.props.length}
+            onChange={this.props.onClick}
+            onKeyPress={this.props.clickEnter}
+          ></input>
+          <span></span>
+        </div>
       </div>
-      <div>
-        <input id={this.props.minID} type="number"
-          className="btn-level" defaultValue={this.props.length}
-          onChange={this.props.onClick} onKeyPress={this.props.clickEnter}>
-        </input>
-        <span></span>
-      </div>
-    </div>
-  )
+    );
+  }
 }
-};
