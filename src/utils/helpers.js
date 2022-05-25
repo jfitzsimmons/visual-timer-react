9655aa2f4de1a79b27e897e81b6f68b57adfb50a
jfitzsimmons <jfitzcode@gmail.com>
2022-05-25 10:24:05 -0500

    env var add edit, all auth

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28,3 +28,11 @@ export const debounce = (func, wait, immediate) => {
     if (callNow) func.apply(context, args);
   };
 };
+
+export const fbDbRestApiconfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
